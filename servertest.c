12b3a9012a1eff323897c81b3a420adad5961633
Ricards Briedis <ricix99@gmail.com>
2021-05-11 17:43:54 +0300

    Slight improvements to servertest.c

--- servertest.c
+++ servertest.c
@@ -5,24 +5,38 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
-#define MAX 80
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+#define MAX_CLIENTS 255 
 #define PORT 3000 
-#define SA struct socketaddr
+#define HOST "localhost"
+#define SHARED_MEMORY_SIZE 1024
+#define BUFFER_SIZE 1024
 
-
+char* shared_memory = NULL;
+int* client_count = NULL;
+int* shared_data = NULL;
+void get_shared_memory();
+void gameloop();
+void start_network();
+void process_client(int id, int sock);
 // comms between server and client
 void infchat(int sock)
 {
-	char buffer[MAX];
+	char buffer[BUFFER_SIZE];
 	int n;
 	// infinite loop
 	for (;;) {
-		bzero(buffer, MAX);
+		bzero(buffer, BUFFER_SIZE);
 		// copy message from client in buffer
 		read(sock, buffer, sizeof(buffer));
 		// print buffer with client contents
 		printf("From client: %s\t To client : ", buffer);
-		bzero(buffer, MAX);
+		bzero(buffer, BUFFER_SIZE);
 		n = 0;
 		// copies server message in buffer
 		while ((buffer[n++] = getchar()) != '\n');
@@ -38,6 +52,15 @@ void infchat(int sock)
 
 int main()
 {
+	int pid = 0; int i;
+	printf("SERVER started!\n");
+	get_shared_memory();
+	pid = fork();
+	if(pid == 0){
+	start_network();
+	}else{
+	gameloop();
+	}
 	int sock, conn, len;
 	struct sockaddr_in serveraddr, cli;
 	// creating socket
