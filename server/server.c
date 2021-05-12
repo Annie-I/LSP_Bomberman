2343f570b18362ed43f1ee5af400bb59a158dbeb
Ricards Briedis <ricix99@gmail.com>
2021-05-12 03:25:54 +0300

    changed server.c

--- server/server.c
+++ server/server.c
@@ -1,119 +1,87 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/wait.h>
 #include <string.h>
-#include <errno.h>
+#include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <sys/mman.h>
 
-#include "../functions/server_functions.h"
-
-#define MAX_CLIENTS 10
-#define PORT 3000
-#define SHARED_MEMORY_SIZE 1024
-
-char* shared_memory = NULL;
-int* client_count = NULL;
-int* shared_data = NULL;
-
-void get_shared_memory(){
- shared_memory = mmap(NULL, SHARED_MEMORY_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS, -1, 0);
- client_count = (int*) shared_memory;
- shared_data = (int*) (shared_memory + sizeof(int));
-}
-void gameloop(){
- printf("Starting game loop!\n");
- int i=0;
- while(1){
-  for(i=0; i<*client_count; i++){
-   shared_data[MAX_CLIENTS+i] += shared_data[i];
-   shared_data[i]=0;
-  }
-  sleep(1);
- }
-}
-void start_network(){
- int main_socket;
- struct sockaddr_in server_address;
- int client_socket;
- struct sockaddr_in client_address;
- int client_address_size = sizeof(client_address);
- 
- main_socket = socket(AF_INET, SOCK_STREAM, 0);
- if(main_socket<0)
+int main (int argc, char *argv[])
+{
+ int socket_desc, client_sock, c, read_size;
+ struct sockaddr_in server, client;
+ char client_message[2000];
+ socket_desc=socket(AF_INET, SOCK_STREAM, 0);
+ if (socket_desc == -1)
  {
- printf("Error opening main server socket!\n"); exit(1);
+ printf("Could not create socket");
  }
- printf("Main socket created!\n");
+ puts("Socket created");
+ server.sin_family = AF_INET;
+ server.sin_addr.s_addr = INADDR_ANY;
+ server.sin_port=htons(3000);
  
- server_address.sin_family = AF_INET;
- server_address.sin_addr.s_addr=INADDR_ANY;
- server_address.sin_port = htons(PORT);
- 
- if(bind(main_socket, (struct sockaddr*) &server_address, sizeof(server_address))<0)
+ if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server))<0)
  {
- printf("Error listening to socket!\n"); exit(1); }
- printf("Main socket is listening!\n");
- 
- while(1){
-  int new_client_id = 0;
-  int cpid = 0;
-  //waiting for clients
-  client_socket = accept(main_socket, (struct sockaddr*) &client_address, &client_address_size);
-  if(client_socket<0){
-   printf("Error accepting client connection! ERRNO=%d\n", errno); continue; }
-  new_client_id = *client_count;
-  *client_count +=1;
-  cpid= fork();
-  
-  if(cpid==0){
-   close(main_socket);
-   cpid=fork();
-   if(cpid==0){
-   process_client(new_client_id, client_socket);
-   exit(0);
-   } else {
-   wait(NULL);
-   printf("Successfully orphaned client %d\n", new_client_id);
-   exit(0);
-  }
- } else {
-  close(client_socket);
+ perror("bind failed. Error");
+ return 1;
  }
-}
-}
-void process_client(int id, int socket){
- int i=0;
- char in[1];
- char out[100];
- printf("Processing client id = %d, socket = %d\n", id, socket);
- printf("CLIENT count %d\n", *client_count);
+ puts("bind done");
  
- while(1){
-  read(socket, in, 1);
-  if(in[0]>47&&in[0]<58){
-   sprintf(out, "CLIENT %d SUM = %d\n", id, shared_data[MAX_CLIENTS+id]);
-   write(socket, out, strlen(out));
-   i = (int)in[0] -48;
-   printf("CLIENT %d read number %d\n", id, i);
-   shared_data[id]=i;
-   }
-  }
+ listen(socket_desc, 3);
+ 
+ puts("Waiting for incoming connections");
+ c=sizeof(struct sockaddr_in);
+ 
+ client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+ if(client_sock<0)
+ {
+ perror("accept failed");
+ return 1;
  }
-
-
-int main(){
- int pid = 0;
- int i;
- printf("SERVER started!\n");
- get_shared_memory();
- pid = fork();
- if(pid == 0){
-  start_network();
- } else {
-  gameloop();
+ puts("connection accepted");
+ 
+ while ((read_size=recv(client_sock, client_message, 2000, 0)) > 0)
+ {
+ write(client_sock, client_message, strlen(client_message));
+ }
+ if(read_size==0)
+ {
+ puts("client disconnected");
+ fflush(stdout);
+ }
+ else if(read_size == -1)
+ {
+ perror("recv failed");
  }
  return 0;
 }
+
+void *connection_handler(void *socket_desc)
+{
+int sock = *(int*)socket_desc;
+int read_size;
+char *message, client_message[2000];
+
+message="Hello\n";
+write(sock, message, strlen(message));
+
+message="Write something to server";
+write(sock, message, strlen(message));
+
+while((read_size=recv(sock, client_message, 2000, 0))>0)
+{
+write(sock, client_message, strlen(client_message));
+}
+
+if(read_size==0)
+{
+puts("Client disconnected");
+fflush(stdout);
+}
+else if(read_size==-1)
+{
+perror("recv failed");
+}
+free(socket_desc);
+return 0;
+}
+
